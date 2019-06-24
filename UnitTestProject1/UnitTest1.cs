2e9d8fcd7b6ac5dc5ac897b45bfbb1f944db3f53
Арсений Сидоров <aasidorov_6@edu.hse.ru>
2019-06-24 10:00:04 +0500

    Добавьте файлы проекта.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using task3;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestInCircleNotInParabola()
+        {
+            Assert.AreEqual(7, Program.Area(0, 2));
+        }
+
+        [TestMethod]
+        public void TestInCircleNotInParabola2()
+        {
+            Assert.AreEqual(7, Program.Area(0, 1.1));
+        }
+        [TestMethod]
+        public void TestInCircleInParabola()
+        {
+            Assert.AreEqual(-1, Program.Area(0, 1));
+        }
+        [TestMethod]
+        public void TestNotInCircleNotInParabola()
+        {
+            Assert.AreEqual(7, Program.Area(5, 0));
+        }
+        [TestMethod]
+        public void TestNotInCircleInParabola()
+        {
+            Assert.AreEqual(7, Program.Area(0, -3));
+        }
+        [TestMethod]
+        public void TestInCircleInParabola2()
+        {
+            Assert.AreEqual(-0.5, Program.Area(0, 0.5));
+        }
+    }
+}
---
+++ task3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PracticeLibrary;
+
+namespace task3
+{
+    public class Program
+    {
+        
+        public static double Area(double x, double y)
+        {
+            if (x * x + (y - 1) * (y - 1) <= 1 && y <= 1 - x * x) return x - y;
+            else return x * y + 7;
+        }
+        static void Main(string[] args)
+        {
+            double x = Inputs.Double("Введите координату x: ");
+            double y = Inputs.Double("Введите координату y: ");
+            Console.WriteLine($"u = {Area(x, y)}");
+            Console.ReadKey();
+
+        }
+
+        
+    }
+}
